ed04e24dc3504ce803c9ae0e3e7a145104a3503d
timalsinas768 <timalsinas768@gmail.com>
2023-03-21 18:14:58 +0545

    search bar done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,29 +6,50 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
  
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   async function getApi() {
     try {
       await axios.get("https://yts.mx/api/v2/list_movies.json?quality=3D").then(res => {
         setMovies(res.data.data.movies);
+        // console.log(movies);
       })
     }
     catch (error) {
       console.log(error);
     }
   }
+// useEffect(()=>{
+// getApi();
+// },[])
 
   useEffect(() => {
-    getApi();
-  }, [])
+    if(inputValue===""){
+     getApi(); 
+     } 
+  else{
+  const filtered=movies.filter((movie)=>movie.title.includes(inputValue));
+  console.log("Filtered",filtered);
+  setMovies(filtered);
+  }
+
+  }, [inputValue])
+
 
 
   return (
     <Layout>
+      <div className="search-header">
+      <h1>This is all Movies</h1>
+     <div className="one-header">
+     <h3>Search</h3>
+      <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
+     </div>
+      </div>
       <div className='main-div'>
         {
-          movies && movies.map((m) => (
+          movies.map((m) => (
             <div key={m.id} className="card-movie">
               <Link to={`/details/${m.id}`}>
               <div className="img-card" >
